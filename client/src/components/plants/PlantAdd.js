c20c0305c806608826c56b5f55a0616e41a55d05
James Kuligowski <jdkuligowski@gmail.com>
2022-05-16 20:37:39 +0100

    committing draft add-plant functionality

--- client/src/components/plants/PlantAdd.js
+++ client/src/components/plants/PlantAdd.js
@@ -3,7 +3,32 @@
 
 const PlantAdd = () => {
   return (
-    <h1>PlantAdd</h1>
+    <section className='form-page'>
+    <form className='form-detail'>
+      <h1>Add Plant</h1>
+      {/* Name */}
+      <label htmlFor='name'>Name</label>
+      <input type='text' name='name' className='input' placeholder='Name' />
+      {/* {errors && <p className = 'denied-text'>Please input username</p>} */}
+      {/* Scientific name */}
+      <label htmlFor='name'>Scientific name</label>
+      <input type='text' name='sci-name' className='input' placeholder='Scientific name' />
+
+      {/* Origin */}
+      <label htmlFor='origin'>Origin</label>
+      <input type='text' name='origin' className='input' placeholder='Origin' />
+      {/* {errors && <p className = 'denied-text'>Please input email</p>} */}
+      {/* Image */}
+      <label htmlFor='image'>Image</label>
+      <input type='text' name='image' className='input' placeholder='Image'/>
+      {/* {errors && <p className = 'denied-text'>Please input password</p>} */}
+      {/* Password Confirmation */}
+      <label htmlFor='description'>Description</label>
+      <textarea name='description' className='input' placeholder='Description' />
+      {/* Submit */}
+      <button type='submit'>Add</button>
+    </form>
+  </section>
   )
 }
 

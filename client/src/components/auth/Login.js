1c2a63a2da595c9234a4134e2cd98332d0b9c298
James Kuligowski <jdkuligowski@gmail.com>
2022-05-16 20:31:18 +0100

    adding login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,7 +2,20 @@
 
 const Login = () => {
   return (
-    <h1>login</h1>
+    <section className='form-page'>
+    <form className='form-detail'>
+      <h1>Login</h1>
+      {/* Email */}
+      <label htmlFor='email'>Email</label>
+      <input type='email' name='email' className='input' placeholder='Email'/>
+      {/* Password */}
+      <label htmlFor='password'>Password</label>
+      <input type='password' name='password' className='input' placeholder='Password'/>
+      {/* {errors && <p className = 'denied-text'>Unauthorised</p>} */}
+      {/* Submit */}
+      <button type = 'submit'>Login</button>
+    </form>
+  </section>
   )
 }
 

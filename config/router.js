6ab40c8a447a50e4d7c745ff3f48b28f48e3f56d
Robert Morgan <robert.morgan0703@gmail.com>
2022-05-17 12:20:40 +0100

    single route

--- config/router.js
+++ config/router.js
@@ -1,11 +1,13 @@
 import express from 'express'
 
-import { showPlants } from '../controllers/AllPlants.js'
+import { showPlants, showSinglePlant } from '../controllers/AllPlants.js'
 
 const router = express.Router()
 
 router.route('/plants')
   .get(showPlants)
+  .get(showSinglePlant)
+
 
 
 export default router
--- controllers/AllPlants.js
+++ controllers/AllPlants.js
@@ -1,8 +1,26 @@
 import Plant from '../models/plants.js'
 
-// Get Plants
+// ?GET REQUESTS
+// Get All Plants
 export const showPlants = async(req, res) => {
-  const plants = await Plant.find()
-  console.log({ plants })
-  return res.status(200).json(plants)
+  try {
+    const plants = await Plant.find()
+    console.log({ plants })
+    return res.status(200).json(plants)
+  } catch (error) {
+    return res.status(400).json(error)
+  }
 }
+
+// Get Single Plants
+export const showSinglePlant = async(req, res) => {
+  try {
+    const { id } = req.params
+    const plant = await Plant.findById(id)
+    if (!plant) throw new Error('Plant not found')
+    return res.status(200).json(plant)
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+}
+

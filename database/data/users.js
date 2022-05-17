94b750a6542990037fd2b1e38009bf9c464fb264
Jack <jackbdrobinson@gmail.com>
2022-05-17 14:58:54 +0100

    admin data added

---
+++ database/data/users.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    username: 'jdkuligowski',
+    email: 'james@mail.com',
+    password: '123',
+    passwordConfirmation: '123',
+  },
+  {
+    username: 'robertM',
+    email: 'rob@mail.com',
+    password: '248',
+    passwordConfirmation: '248',
+  },
+  {
+    username: 'jackR',
+    email: 'jack@mail.com',
+    password: '135',
+    passwordConfirmation: '135',
+  }
+]

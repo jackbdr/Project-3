25cba4c7d6c596cdb9a55056fd5ce5556ac64c85
James Kuligowski <jdkuligowski@gmail.com>
2022-05-17 15:37:06 +0100

    First 3 plants pushed

--- database/data/plants.js
+++ database/data/plants.js
@@ -1,10 +1,101 @@
 export default [
   {
-    name: 'cheese plant',
-    sciName: 'Monstera deliciosa',
+    name: 'Broadleaf Lady Palm',
+    sciName: 'Rhapis Excelsa',
+    image: '',
+    origin: 'China',
+    watering: 'Rhapis excelsa demands soil that is constantly moist, but never soggy. Do not permit standing water to collect, as it will rot the root system.',
+    temp: '-6-37°C',
+    tempDetail: 'It is almost impossible to upset the temperature of the Broadleaf Lady Palm. It will happily tolerate a broad range of temperatures.',
+    light: 'Enjoys indirect sunlight. Full sunlight will injur the leaves.',
+    fertilizer: 'This plant rarely requires fertilizer',
+    pruning: 'Should be carried out as required to prevent plant from becoming overgrown.',
+    problems: [
+      {
+        prob1: 'Lower leaves become discoloured with age.',
+        prob2: 'Brown leaves are caused by extreme heat or lack of watering.',
+        prob3: 'Black leaf tips are associated with over watering or over fertilizing.',
+      }
+    ],
+    solutions: [
+      {
+        sol1: 'These should be pruned for the plants health.',
+        sol2: 'Fix the problem by trimming the brown leaves from the plant.',
+        sol3: 'Fix the problem by trimming the black leaves from the plant.',
+      }
+    ],
+    maxGrowth: '4 metres',
+    annualGrowth: '20-30cm',
+    poisonous: 'Non toxic for cats and dogs.',
+    easeRating: 4,
+    description: 'The Broadleaf Lady Palm is a multi-stemmed palm plant that has become increasingly popular around the world. The plant has proven successful in removing airborne toxins within the home, including ammonia, formaldehyde, xylene, and carbon dioxide. Unlike other plants, who simply make oxygen, Rhapis excelsa will actually make the air in your home cleaner and safer to breathe. It is widely known as one of the easiest house plants to grow in the world. Distinct in nature, this palm survives in households around the world, and has been grown as a house plant for over 500 years in Japan. Indoor plants are kept in check through pruning.',
+    priceRange: '£30-120',
+    buyLink: '',
   },
   {
-    name: 'snakes plant',
+    name: 'Cheese Plant',
+    sciName: 'Monstera Deliciosa',
+    image: '',
+    origin: 'Central America',
+    watering: 'Between watering times allow the soil to become dry to the touch within the top couple of inches or so of soil',
+    temp: '18-27°C',
+    tempDetail: 'Lower than 18°C will slow growth and below 10°C will stop growth',
+    light: 'A fairly bright room or bright with plenty of shade is best. Direct sunlight will damage the leaves and not enough light can slow growth down.',
+    fertilizer: 'Fertilizing once a monthwith a diluted solution would be preferred.',
+    pruning: 'This plant can easily get out of hand once it has been growing for over 3 years or so and will need cutting back.',
+    problems: [
+      {
+        prob1: 'Yellow leaves can be caused through over-watering.',
+        prob2: 'Brown leaves can be caused by low humidity and dry air.',
+        prob3: 'Leaves may not form slits or holes.',
+      }
+    ],
+    solutions: [
+      {
+        sol1: 'Watering less should help, if this doesn\'t, the soil may need fertilizer.',
+        sol2: 'Changing the pot could help, or adjusting the room\'s humidity.',
+        sol3: 'This is usually due to lack of something which will be light, not enough water or not enough fertilizer. ',
+      }
+    ],
+    maxGrowth: '3 metres',
+    annualGrowth: '30-60cm',
+    poisonous: 'Non toxic for cats and dogs.',
+    easeRating: 5,
+    description: 'Monstera deliciosa, the Swiss cheese plant or split-leaf philodendron is a species of flowering plant native to tropical forests of southern Mexico, south to Panama. It has been introduced to many tropical areas, and has become a mildly invasive species in Hawaii, Seychelles, Ascension Island and the Society Islands. It is very widely grown in temperate zones as a houseplant.',
+    priceRange: '£10-100',
+    buyLink: '',
+  },
+  {
+    name: 'Snake Plant',
     sciName: 'Dracaena trifasciata',
+    image: '',
+    origin: 'West Africa',
+    watering: 'Because this plant is a succulent it stores water within its foliage, so it is not necessary to keep the soil damp. Water from spring to autumn when the soil becomes dry to the touch and during the winter only once a month. Be careful not to over water as this can cause the root and base of the plant to rot.',    
+    temp: '15-24°C',
+    tempDetail: 'Average warmth is fine and no lower than 10°C',
+    light: 'The mother in laws tongue is well known for coping with direct sun and low light conditions, although bright light conditions with some sun light and shade is preferred.',
+    fertilizer: 'During Spring to Autumn, feed with succulent fertilizer once a month.',
+    pruning: 'This plant can easily get out of hand once it has been growing for over 3 years or so and will need cutting back.',
+    problems: [
+      {
+        prob1: 'Base rot can be caused by over-watering in cold conditions',
+        prob2: '',
+        prob3: '',
+      }
+    ],
+    solutions: [
+      {
+        sol1: 'Remove the most affected parts or discard the whole plant if all the base is completely affected',
+        sol2: '',
+        sol3: '',
+      }
+    ],
+    maxGrowth: '1.5 metres',
+    annualGrowth: '5-15cm',
+    poisonous: 'Mildly toxic for cats and dogs.',
+    easeRating: 4,
+    description: 'Dracaena trifasciata is a species of flowering plant in the family Asparagaceae, native to tropical West Africa from Nigeria east to the Congo. It is most commonly known as the snake plant, Saint George\'s sword, mother-in-law\'s tongue, and viper\'s bowstring hemp, among other names. Until 2017, it was known under the synonym Sansevieria trifasciata.',
+    priceRange: '£10-100',
+    buyLink: '',
   }
 ] 
--- models/plants.js
+++ models/plants.js
@@ -6,6 +6,28 @@ import mongooseUniqueValidator from 'mongoose-unique-validator'
 const plantSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   sciName: { type: String, required: true, unique: true },
+  image: { type: String, required: true },
+  origin: { type: String, required: true },
+  watering: { type: String, required: true },
+  temp: { type: String, required: true },
+  tempDetail: { type: String, required: true },
+  light: { type: String, required: true },
+  fertilizer: { type: String, required: true },
+  pruning: { type: String, required: true },
+  problems: [ 
+    {
+      prob1: { type: String },
+      prob2: { type: String },
+      prob3: { type: String },
+    }
+  ],
+  maxGrowth: { type: String, required: true },
+  annualGrowth: { type: String, required: true },
+  poisonous: { type: String, required: true },
+  easeRating: { type: String, required: true },
+  description: { type: String, required: true },
+  priceRange: { type: String, required: true },
+  buyLink: { type: String, required: true },
 })
 
 

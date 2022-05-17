08e4686548699b06ae49d3057df46f55644897b5
Robert Morgan <robert.morgan0703@gmail.com>
2022-05-17 14:41:04 +0100

    working login/register

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,12 +1,28 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
-import 'dotenv/config'
 
+import User from '../models/users.js'
 
 export const registerUser = async(req, res) => {
-  console.log('register route')
+  const { body } = req
+  try {
+    const newUser = await User.create(body)
+    return res.status(200).json({ message: `Welcome ${newUser.username}` })
+  } catch (error) {
+    return res.status(400).json(error)
+  }
 }
 
 export const loginUser = async(req, res) => {
-  console.log('login route')
+  const { username, password } = req.body
+  try {
+    const userLogin = await User.findOne({ username: username })
+    if (!userLogin || !userLogin.validatePassword(password)) throw new Error
+
+    const token = jwt.sign({ sub: userLogin._id }, process.env.SECRET, { expiresIn: '1h' })
+    return res.status(200).json({ message: `Welcome back ${userLogin.username}`, token: token })
+  } catch (error) {
+    return res.status(422).json({ message: 'Unauthorised' })
+  }
+
 }

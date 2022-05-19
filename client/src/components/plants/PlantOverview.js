f14eb2bd6c72d18559cfbe2e084bdea7f82b31c7
Jack <jackbdrobinson@gmail.com>
2022-05-19 14:12:10 +0100

    setLoading & setErrors

--- client/src/components/plants/PlantOverview.js
+++ client/src/components/plants/PlantOverview.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import { Link, useNavigate } from 'react-router-dom'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
 const PlantOverview = () => {
 
   const [plants, setPlants] = useState([])
+  const [errors, setErrors] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+
+    // !isUserAuth() && navigate('/login')
+
     const getPlants = async () => {
       try {
         const { data } = await axios.get('/api/plants')
+        console.log(data)
         setPlants(data)
-        console.log(data[0].image)
+        setLoading(false)
       } catch (err) {
         console.log(err)
+        setErrors(true)
       }
     }
     getPlants()

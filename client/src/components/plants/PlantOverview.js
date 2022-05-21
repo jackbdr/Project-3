1c5d2a5fc463d0df52dabd95101d9fdbeba91d98
Jack <jackbdrobinson@gmail.com>
2022-05-21 11:08:26 +0100

    button filters

--- client/src/components/plants/PlantOverview.js
+++ client/src/components/plants/PlantOverview.js
@@ -8,19 +8,33 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 
+import { Can1, Can1Colour, Can2, Can2Colour, Can3, Can3Colour, Can4, Can4Colour, Can5, Can5Colour } from '../helpers/Buttons'
+import { Sun1, Sun1Colour, Sun2, Sun2Colour, Sun3, Sun3Colour, Sun4, Sun4Colour, Sun5, Sun5Colour } from '../helpers/Buttons'
 import { LightBulb1Off, LightBulb1On, LightBulb2Off, LightBulb2On, LightBulb3Off, LightBulb3On, LightBulb4Off, LightBulb4On, LightBulb5Off, LightBulb5On } from '../helpers/Buttons'
+import { Thumb1, Thumb1Colour, Thumb2, Thumb2Colour, Thumb3, Thumb3Colour, Thumb4, Thumb4Colour, Thumb5, Thumb5Colour } from '../helpers/Buttons'
 
 
 const PlantOverview = () => {
 
   const [plants, setPlants] = useState([])
-  const [filters, setFilters] = useState({
+  const [waterFilter, setWaterFilter] = useState({
     water: '',
+  })
+  const [lightFilter, setLightFilter] = useState({
     light: '',
+  })
+  const [brightnessFilter, setBrightnessFilter] = useState({
     brightness: '',
+  })
+  const [easeFilter, setEaseFilter] = useState({
     ease: '',
   })
+
+  const [cans, setCans] = useState(false)
+  const [suns, setSuns] = useState(false)
   const [bulbs, setBulbs] = useState(false)
+  const [thumbs, setThumbs] = useState(false)
+
 
 
   const [errors, setErrors] = useState(false)
@@ -44,23 +58,71 @@ const PlantOverview = () => {
     getPlants()
   }, [])
 
-  const handleChange = (e) => {
-    const newObj = {
-      ...filters,
-      [e.target.name]: e.target.value
+  const handleCanChange = (e) => {
+    if (cans === e.target.id) {
+      setCans(false)
+      setWaterFilter({ water: '' })
+    } else {
+      const newObj = {
+        ...waterFilter,
+        [e.target.name]: e.target.value
+      }
+      setWaterFilter(newObj)
+      setCans(e.target.id)
+      console.log(e.target.id)
     }
-    setFilters(newObj)
-    setBulbs(e.target.id)
-    console.log(e.target.id)
   }
 
+  const handleSunChange = (e) => {
+    if (suns === e.target.id) {
+      setSuns(false)
+      setLightFilter({ light: '' })
+    } else {
+      const newObj = {
+        ...lightFilter,
+        [e.target.name]: e.target.value
+      }
+      setLightFilter(newObj)
+      setSuns(e.target.id)
+      console.log(e.target.id)
+    }
+  }
 
+  const handleBulbChange = (e) => {
+    if (bulbs === e.target.id) {
+      setBulbs(false)
+      setBrightnessFilter({ brightness: '' })
+    } else {
+      const newObj = {
+        ...brightnessFilter,
+        [e.target.name]: e.target.value
+      }
+      setBrightnessFilter(newObj)
+      setBulbs(e.target.id)
+      console.log(e.target.id)
+    }
+  }
+
+  const handleThumbChange = (e) => {
+    if (thumbs === e.target.id) {
+      setThumbs(false) 
+      setEaseFilter({ ease: '' })
+    } else {
+      const newObj = {
+        ...easeFilter,
+        [e.target.name]: e.target.value
+      }
+      setEaseFilter(newObj)
+      setThumbs(e.target.id)
+      console.log(e.target.id)
+    }
+  }
 
   const handleFilter = () => {
     if (plants.length) {
       const filtered = plants.filter(plant => {
         // return plant.waterCategory === filters.water && plant.sunlightFilter === filters.light && plant.brightnessFilter === filters.brightness && plant.seededEaseRating === filters.ease 
-        return (plant.waterCategory.toString().includes(filters.water) && plant.sunlightFilter.toString().includes(filters.light) && plant.brightnessFilter.toString().includes(filters.brightness) && plant.seededEaseRating.toString().includes(filters.ease))
+        return (plant.waterCategory.toString().includes(waterFilter.water) && plant.sunlightFilter.toString().includes(lightFilter.light) && plant.brightnessFilter.toString().includes(brightnessFilter.brightness) && plant.seededEaseRating.toString().includes(easeFilter.ease))
       })
       // console.log(filtered)
       // setFilteredPLants(filtered)
@@ -69,13 +131,22 @@ const PlantOverview = () => {
   }
 
   const reset = () => {
-    setFilters({
+    setWaterFilter({
       water: '',
+    })
+    setLightFilter({
       light: '',
+    })
+    setBrightnessFilter({
       brightness: '',
+    })
+    setEaseFilter({
       ease: '',
     })
+    setCans(false)
+    setSuns(false)
     setBulbs(false)
+    setThumbs(false)
   }
 
 
@@ -86,61 +157,121 @@ const PlantOverview = () => {
           <div className='cans label-icon'>
             <p>Water</p>
             <div className='icons-container'>
-              <button id='can-1' className='can' name='water' value={1} onClick={handleChange}></button>
-              <button id='can-2' className='can' name='water' value={2} onClick={handleChange}></button>
-              <button id='can-3' className='can' name='water' value={3} onClick={handleChange}></button>
-              <button id='can-4' className='can' name='water' value={4} onClick={handleChange}></button>
-              <button id='can-5' className='can' name='water' value={5} onClick={handleChange}></button>
+              {cans === 'can1' ?
+                <Can1Colour handleCanChange={handleCanChange} />
+                :
+                <Can1 handleCanChange={handleCanChange} />
+              }
+              {cans === 'can2' ?
+                <Can2Colour handleCanChange={handleCanChange} />
+                :
+                <Can2 handleCanChange={handleCanChange} />
+              }
+              {cans === 'can3' ?
+                <Can3Colour handleCanChange={handleCanChange} />
+                :
+                <Can3 handleCanChange={handleCanChange} />
+              }
+              {cans === 'can4' ?
+                <Can4Colour handleCanChange={handleCanChange} />
+                :
+                <Can4 handleCanChange={handleCanChange} />
+              }
+              {cans === 'can5' ?
+                <Can5Colour handleCanChange={handleCanChange} />
+                :
+                <Can5 handleCanChange={handleCanChange} />
+              }
             </div>
           </div>
           <div className='suns label-icon'>
             <p>Sunlight</p>
             <div className='icons-container'>
-              <button id='sun1' className='sun' name='light' value={1} onClick={handleChange}></button>
-              <button id='sun2' className='sun' name='light' value={2} onClick={handleChange}></button>
-              <button id='sun3' className='sun' name='light' value={3} onClick={handleChange}></button>
-              <button id='sun4' className='sun' name='light' value={4} onClick={handleChange}></button>
-              <button id='sun5' className='sun' name='light' value={5} onClick={handleChange}></button>
+              {suns === 'sun1' ?
+                <Sun1Colour handleSunChange={handleSunChange} />
+                :
+                <Sun1 handleSunChange={handleSunChange} />
+              }
+              {suns === 'sun2' ?
+                <Sun2Colour handleSunChange={handleSunChange} />
+                :
+                <Sun2 handleSunChange={handleSunChange} />
+              }
+              {suns === 'sun3' ?
+                <Sun3Colour handleSunChange={handleSunChange} />
+                :
+                <Sun3 handleSunChange={handleSunChange} />
+              }
+              {suns === 'sun4' ?
+                <Sun4Colour handleSunChange={handleSunChange} />
+                :
+                <Sun4 handleSunChange={handleSunChange} />
+              }
+              {suns === 'sun5' ?
+                <Sun5Colour handleSunChange={handleSunChange} />
+                :
+                <Sun5 handleSunChange={handleSunChange} />
+              }
             </div>
           </div>
           <div className='bulbs label-icon'>
             <p>Brightness</p>
             <div className='icons-container'>
               {bulbs === 'bulb1' ?
-                <LightBulb1On handleChange={handleChange} />
+                <LightBulb1On handleBulbChange={handleBulbChange} />
                 :
-                <LightBulb1Off handleChange={handleChange} />
+                <LightBulb1Off handleBulbChange={handleBulbChange} />
               }
               {bulbs === 'bulb2' ?
-                <LightBulb2On handleChange={handleChange} />
+                <LightBulb2On handleBulbChange={handleBulbChange} />
                 :
-                <LightBulb2Off handleChange={handleChange} />
+                <LightBulb2Off handleBulbChange={handleBulbChange} />
               }
               {bulbs === 'bulb3' ?
-                <LightBulb3On handleChange={handleChange} />
+                <LightBulb3On handleBulbChange={handleBulbChange} />
                 :
-                <LightBulb3Off handleChange={handleChange} />
+                <LightBulb3Off handleBulbChange={handleBulbChange} />
               }
               {bulbs === 'bulb4' ?
-                <LightBulb4On handleChange={handleChange} />
+                <LightBulb4On handleBulbChange={handleBulbChange} />
                 :
-                <LightBulb4Off handleChange={handleChange} />
+                <LightBulb4Off handleBulbChange={handleBulbChange} />
               }
               {bulbs === 'bulb5' ?
-                <LightBulb5On handleChange={handleChange} />
+                <LightBulb5On handleBulbChange={handleBulbChange} />
                 :
-                <LightBulb5Off handleChange={handleChange} />
+                <LightBulb5Off handleBulbChange={handleBulbChange} />
               }
             </div>
           </div>
           <div className='thumbs label-icon'>
             <p>Ease rating</p>
             <div className='icons-container'>
-              <button className='thumb' name='ease' value={1} onClick={handleChange}></button>
-              <button className='thumb' name='ease' value={2} onClick={handleChange}></button>
-              <button className='thumb' name='ease' value={3} onClick={handleChange}></button>
-              <button className='thumb' name='ease' value={4} onClick={handleChange}></button>
-              <button className='thumb' name='ease' value={5} onClick={handleChange}></button>
+              {thumbs === 'thumb1' ?
+                <Thumb1Colour handleThumbChange={handleThumbChange} />
+                :
+                <Thumb1 handleThumbChange={handleThumbChange} />
+              }
+              {thumbs === 'thumb2' ?
+                <Thumb2Colour handleThumbChange={handleThumbChange} />
+                :
+                <Thumb2 handleThumbChange={handleThumbChange} />
+              }
+              {thumbs === 'thumb3' ?
+                <Thumb3Colour handleThumbChange={handleThumbChange} />
+                :
+                <Thumb3 handleThumbChange={handleThumbChange} />
+              }
+              {thumbs === 'thumb4' ?
+                <Thumb4Colour handleThumbChange={handleThumbChange} />
+                :
+                <Thumb4 handleThumbChange={handleThumbChange} />
+              }
+              {thumbs === 'thumb5' ?
+                <Thumb5Colour handleThumbChange={handleThumbChange} />
+                :
+                <Thumb5 handleThumbChange={handleThumbChange} />
+              }
             </div>
             <button className='reset' onClick={reset}>Reset</button>
           </div>
@@ -152,6 +283,7 @@ const PlantOverview = () => {
           :
           <div className='plants-container'>
             <h3>All plants</h3>
+            <hr style={{color: "green"}}/>
             <Container className='bstrap-container'>
               <Row className='bstrap-row'>
                 {handleFilter().map(plant => {

e00021c93681e474c298350c336a263661e5cec7
James Kuligowski <jdkuligowski@gmail.com>
2022-05-22 11:10:58 +0100

    Login page styled

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 const Login = () => {
 
   const navigate = useNavigate()
 
-  const [ formData, setFormData ] = useState({
+  const [formData, setFormData] = useState({
     email: '',
     password: '',
   })
 
-  const [ errors, setErrors ] = useState(false)
+  const [errors, setErrors] = useState(false)
 
   const setTokenToLocalStorage = (token) => {
     window.localStorage.setItem('project-3-plants', token)
@@ -38,19 +38,28 @@ const Login = () => {
 
   return (
     <section className='form-page'>
-    <form className='form-detail' onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      {/* Email */}
-      <label htmlFor='email'>Email</label>
-      <input type='email' name='email' className='input' placeholder='Email' value = {formData.email} onChange={handleChange}/>
-      {/* Password */}
-      <label htmlFor='password'>Password</label>
-      <input type='password' name='password' className='input' placeholder='Password' value = {formData.password} onChange={handleChange}/>
-      {errors && <p className = 'denied-text'>Please enter the correct login details</p>}
-      {/* Submit */}
-      <button type = 'submit'>Login</button>
-    </form>
-  </section>
+      <form className='form-detail' onSubmit={handleSubmit}>
+        <h1>Login to your account</h1>
+        <hr />
+        {/* Email */}
+        <label htmlFor='email'></label>
+        <input type='email' name='email' className='input' placeholder='Email' value={formData.email} onChange={handleChange} />
+        {/* Password */}
+        <label htmlFor='password'></label>
+        <input type='password' name='password' className='input' placeholder='Password' value={formData.password} onChange={handleChange} />
+        {errors && <p className='denied-text'>Please enter the correct login details</p>}
+        {/* Submit */}
+        <button type='submit'>Sign in</button>
+      </form>
+      <section className='new-user'>
+        <h1>New to seeded?</h1>
+        <h4>Sign up for tips tricks and everything you need to start your plant-keeping journey</h4>
+        <Link to={`/register`}>
+          <button>Sign up</button>
+        </Link>
+
+      </section>
+    </section>
   )
 }
 

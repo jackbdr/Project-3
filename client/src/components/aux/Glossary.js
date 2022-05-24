c130d1481281f1700c64f5238dcbcd7a75a1ff43
Jack <jackbdrobinson@gmail.com>
2022-05-24 22:36:14 +0100

    additions

--- client/src/components/aux/Glossary.js
+++ client/src/components/aux/Glossary.js
@@ -1,40 +1,62 @@
 
 const Glossary = () => {
   return (
-    <section className='glossary-wrapper'>
-      <div className='entry'>
-        <h4>Brightness</h4>
-        <p>Brightness refers to how well lit a space is. Some plants can do well in a shady north facing garden like the Dragon Tree, whilst some will need a light environment throughout the day.</p>
-      </div>
-      <hr />
-      <div className='entry'>
-        <h4>Sunlight</h4>
-        <p>Sunlight refers to the amount of direct sunshine a plant enjoys. Whilst plants like to soak up as many rays as possible, others enjoy the sun in patches or short intervals. </p>
-      </div>
-      <hr />
-      <div className='entry'>
-        <h4>Plant Propagation</h4>
-        <p>Plant propagation is the process by which new plants grow from a variety of sources: seeds, cuttings, and other plant parts. Plant propagation can also refer to the man-made or natural dispersal of seeds.</p>
-      </div>
-      <hr />
-      <div className='entry'>
-        <h4>Prune</h4>
-        <p>To prune a plant is to by cut away dead or overgrown branches or stems, especially to encourage growth.</p>
-      </div>
-      <hr />
-      <div className='entry'>
-        <h4>Crown</h4>
-        <p>The crown of a plant refers to the total of an individual plant's aboveground parts, including stems, leaves, and reproductive structures.</p>
-      </div>
-      <hr />
-      <div className='entry'>
-        <h4>Die back</h4>
-        <p>A plant begins to die back when it is under stress. Wilting at the tip of the shoots gradually moves down the stem and to the rest of the plant.</p>
-      </div>
-      <hr />
-      <div className='entry'>
-        <h4>Perennial</h4>
-        <p>A perrenial plant lives for more than two years (e.g. Foxgloves are perennial)</p>
+    <section className='glossary-container'>
+      <div className='entries-container'>
+        <div className='entry'>
+          <h4>North facing garden</h4>
+          <p>North facing gardens are typically in the shade for most of the day, as the house itself is blocking the sun.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>East facing garden</h4>
+          <p>An east facing garden receives sun throughout the morning, with the sun rising over the bottom of the garden.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>South facing garden</h4>
+          <p>A south facing garden receives sun all, day, long. If looking out to the garden, the sun rises on the left and sets on the right.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>West facing garden</h4>
+          <p>A west facing garden gets the afternoon sun along with 'golden hour' and a sunset at the bottom of the garden.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>Brightness</h4>
+          <p>Brightness refers to how well lit a space is. Some plants can do well in a shady north facing garden like the Dragon Tree, whilst some will need a light environment throughout the day.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>Sunlight</h4>
+          <p>Sunlight refers to the amount of direct sunshine a plant enjoys. Whilst plants like to soak up as many rays as possible, others enjoy the sun in patches or short intervals. </p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>Plant Propagation</h4>
+          <p>Plant propagation is the process by which new plants grow from a variety of sources: seeds, cuttings, and other plant parts. Plant propagation can also refer to the man-made or natural dispersal of seeds.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>Prune</h4>
+          <p>To prune a plant is to by cut away dead or overgrown branches or stems, especially to encourage growth.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>Crown</h4>
+          <p>The crown of a plant refers to the total of an individual plant's aboveground parts, including stems, leaves, and reproductive structures.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>Die back</h4>
+          <p>A plant begins to die back when it is under stress. Wilting at the tip of the shoots gradually moves down the stem and to the rest of the plant.</p>
+        </div>
+        <hr />
+        <div className='entry'>
+          <h4>Perennial</h4>
+          <p>A perrenial plant lives for more than two years (e.g. Foxgloves are perennial)</p>
+        </div>
       </div>
     </section>
   )

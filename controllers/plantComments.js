a7b7f9d9905d50d1a577b2a22fd4992f66c0e8c0
James Kuligowski <jdkuligowski@gmail.com>
2022-05-24 11:55:19 +0100

    committing changes in dev branch

--- controllers/plantComments.js
+++ controllers/plantComments.js
@@ -2,27 +2,20 @@ import Plant from '../models/plants.js'
 
 
 // Adding comments - POST request
-
 export const addCommentToPlant = async (req, res) => {
-
   const { id } = req.params
-
   try {
     console.log('hit the comment route')
     const plantToUpdate = await Plant.findById(id)
 
     if (!plantToUpdate) throw new Error('Plant not found')
-
     // Creating a comment with owner
     const commentWithOwner = { ...req.body, addedBy: req.verifiedUser._id }
     console.log(req.body)
-
     // Adding commentWithOwner to the array
     plantToUpdate.comments.push(commentWithOwner)
-
     // Save updated comment
     await plantToUpdate.save()
-
     return res.status(200).json({ message: 'addComment worked' })
   } catch (err) {
     console.log(err)

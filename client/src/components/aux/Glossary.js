cfa00cea3fffa9b27f55e3d68be896fdd923f3a7
Jack <jackbdrobinson@gmail.com>
2022-05-24 21:34:04 +0100

    glossary start

--- client/src/components/aux/Glossary.js
+++ client/src/components/aux/Glossary.js
@@ -1,7 +1,42 @@
 
 const Glossary = () => {
   return (
-    <h1>Plant Glossary</h1>
+    <section className='glossary-wrapper'>
+      <div className='entry'>
+        <h4>Brightness</h4>
+        <p>idjwjifbd</p>
+      </div>
+      <hr />
+      <div className='entry'>
+        <h4>Sunlight</h4>
+        <p>idjwjifbd</p>
+      </div>
+      <hr />
+      <div className='entry'>
+        <h4>Plant Propagation</h4>
+        <p>Plant propagation is the process by which new plants grow from a variety of sources: seeds, cuttings, and other plant parts. Plant propagation can also refer to the man-made or natural dispersal of seeds.</p>
+      </div>
+      <hr />
+      <div className='entry'>
+        <h4>Prune</h4>
+        <p>To prune a plant is to by cut away dead or overgrown branches or stems, especially to encourage growth.</p>
+      </div>
+      <hr />
+      <div className='entry'>
+        <h4>Crown</h4>
+        <p>The crown of a plant refers to the total of an individual plant's aboveground parts, including stems, leaves, and reproductive structures.</p>
+      </div>
+      <hr />
+      <div className='entry'>
+        <h4>Die back</h4>
+        <p>A plant begins to die back when it is under stress. Wilting at the tip of the shoots gradually moves down the stem and to the rest of the plant.</p>
+      </div>
+      <hr />
+      <div className='entry'>
+        <h4>Perennial</h4>
+        <p>A perrenial plant lives for more than two years (e.g. Foxgloves are perennial)</p>
+      </div>
+    </section>
   )
 }
 

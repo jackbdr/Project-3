316fe1414041480139ef7930d65ba1f7c04cb222
Robert Morgan <robert.morgan0703@gmail.com>
2022-05-25 11:23:42 +0100

    glossary filter

--- client/src/components/aux/Glossary.js
+++ client/src/components/aux/Glossary.js
@@ -1,63 +1,79 @@
+import { useState } from 'react'
 
 const Glossary = () => {
+  
+  const [ filter, setFilter ] = useState('')
+
+
+
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+  const terms = [
+  {name: 'Brightness', description: 'Brightness refers to how well lit a space is. Some plants can do well in a shady north facing garden like the Dragon Tree, whilst some will need a light environment throughout the day.'},
+  {name: 'Crown', description: `The crown of a plant refers to the total of an individual plant's aboveground parts, including stems, leaves, and reproductive structures.`},
+  {name: 'Die back', description: 'A plant begins to die back when it is under stress. Wilting at the tip of the shoots gradually moves down the stem and to the rest of the plant.'},
+  {name: 'East facing garden', description: 'An east facing garden receives sun throughout the morning, with the sun rising over the bottom of the garden.'},
+  {name: 'North facing garden', description: 'North facing gardens are typically in the shade for most of the day, as the house itself is blocking the sun.'},
+  {name: 'Perennial', description: `A perrenial plant lives for more than two years (e.g. Foxgloves are perennial)`},
+  {name: 'Plant Propagation', description: 'Plant propagation is the process by which new plants grow from a variety of sources: seeds, cuttings, and other plant parts. Plant propagation can also refer to the man-made or natural dispersal of seeds.'},
+  {name: 'Prune', description: 'To prune a plant is to by cut away dead or overgrown branches or stems, especially to encourage growth.'},
+  {name: 'South facing garden', description: 'A south facing garden receives sun all, day, long. If looking out to the garden, the sun rises on the left and sets on the right.'},
+  {name: 'Sunlight', description: 'Sunlight refers to the amount of direct sunshine a plant enjoys. Whilst plants like to soak up as many rays as possible, others enjoy the sun in patches or short intervals.'},
+  {name: 'West facing garden', description: `A west facing garden gets the afternoon sun along with 'golden hour' and a sunset at the bottom of the garden.`},
+  ]
+
+
+
+
+  const handleClick = (e) => {
+    setFilter(e.target.value)
+  }
+
+  const handleFilter = () => {
+    return terms.filter(term => {
+      return term.name.includes(filter)
+    })
+  }
+
+
   return (
     <section className='glossary-container'>
       <h1>Glossary</h1>
+      <div className="glossary-filter">
+        <button value={''} onClick={handleClick}>All</button>
+        <>
+        {letters.map((letter, index) => {
+          return(
+            letter === filter ? 
+            <button className='button-active'>{letter}</button>
+            :
+            <button key={index} value={letter} onClick={handleClick}>{letter}</button>
+          )
+        })}
+        </>
+      </div>
       <div className='entries-container'>
+        <>
+        {handleFilter().length === 0 ?
         <div className='entry'>
-          <h4>North facing garden</h4>
-          <p>North facing gardens are typically in the shade for most of the day, as the house itself is blocking the sun.</p>
+        <h4>No Entries Found</h4>
         </div>
-        <hr />
-        <div className='entry'>
-          <h4>East facing garden</h4>
-          <p>An east facing garden receives sun throughout the morning, with the sun rising over the bottom of the garden.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>South facing garden</h4>
-          <p>A south facing garden receives sun all, day, long. If looking out to the garden, the sun rises on the left and sets on the right.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>West facing garden</h4>
-          <p>A west facing garden gets the afternoon sun along with 'golden hour' and a sunset at the bottom of the garden.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>Brightness</h4>
-          <p>Brightness refers to how well lit a space is. Some plants can do well in a shady north facing garden like the Dragon Tree, whilst some will need a light environment throughout the day.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>Sunlight</h4>
-          <p>Sunlight refers to the amount of direct sunshine a plant enjoys. Whilst plants like to soak up as many rays as possible, others enjoy the sun in patches or short intervals. </p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>Plant Propagation</h4>
-          <p>Plant propagation is the process by which new plants grow from a variety of sources: seeds, cuttings, and other plant parts. Plant propagation can also refer to the man-made or natural dispersal of seeds.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>Prune</h4>
-          <p>To prune a plant is to by cut away dead or overgrown branches or stems, especially to encourage growth.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>Crown</h4>
-          <p>The crown of a plant refers to the total of an individual plant's aboveground parts, including stems, leaves, and reproductive structures.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>Die back</h4>
-          <p>A plant begins to die back when it is under stress. Wilting at the tip of the shoots gradually moves down the stem and to the rest of the plant.</p>
-        </div>
-        <hr />
-        <div className='entry'>
-          <h4>Perennial</h4>
-          <p>A perrenial plant lives for more than two years (e.g. Foxgloves are perennial)</p>
-        </div>
+        :
+        <>
+        {handleFilter().map((term, index) => {
+          return(
+            <>
+            <div className='entry' key={index}>
+              <h4>{term.name}</h4>
+              <p>{term.description}</p>
+              </div>
+              <hr />
+              </>
+          )
+        })}
+        </>
+        }
+      </>
       </div>
     </section>
   )
